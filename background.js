de1f07b55c907b9cfade709767d99061a3b3253d
Santiago Figueiras <santi@ubykuo.com>
2018-05-17 22:33:16 -0300

    Changed the search to be in the current tab

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.omnibox.onInputEntered.addListener(function (text) {
     chrome.storage.sync.get('queryString', function (o) {
         var newURL = 'https://www.google.com/search?q=' + o.queryString + '%20' + encodeURIComponent(text);
-        chrome.tabs.create({
+        chrome.tabs.update({
             url: newURL
         });
     });
